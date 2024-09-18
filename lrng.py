aabe1d8ec489f535fa3ef2ae7bd47af2026473a6
lorand <lorand77@users.noreply.github.com>
2024-09-18 19:33:55 +0200

    longest streak

--- lrng.py
+++ lrng.py
@@ -1,11 +1,11 @@
 import time
 
-MAX_RAND_INT = 2 ** 16 + 1
+MAX_RAND_INT = 2 ** 31 - 1
 
 def lgc(x):
     m = MAX_RAND_INT
-    a = 75
-    c = 74
+    a = 1583458089
+    c = 0
     return (x * a + c) % m
 
 state = int(time.time()*1_000_000) % MAX_RAND_INT   ## seed 
--- random_numbers.py
+++ random_numbers.py
@@ -1,10 +1,26 @@
 import lrng
 import matplotlib.pyplot as plt
 
+def find_longest_streak(x):
+    if len(x) == 0:
+        return (0, 0)
+    position = 0
+    streak = 1
+    i = 0
+    while i < len(x) - 1:
+        j = i + 1
+        while j < len(x) and x[i] == x[j]:
+            j += 1
+        if j - i > streak:
+            streak = j - i
+            position = i
+        i += j - i
+    return (position, streak)
+
 rolls = []
 counts = [0] * 6
 counts_pairs = [[0] * 6, [0] * 6, [0] * 6, [0] * 6, [0] * 6, [0] * 6]
-for i in range(36000):
+for i in range(10000000):
     roll = lrng.roll_die()
     rolls.append(roll)
     counts[roll - 1] += 1
@@ -22,3 +38,5 @@ print(counts_pairs)
 #plt.hist(rolls, bins = 6)
 # plt.plot([1,2,3,4,5,6],counts,'o-')
 # plt.show()
+
+print(find_longest_streak(rolls))
